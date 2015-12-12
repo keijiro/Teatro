62b728ca6b52cc13dc488dc0155f5449c755c2c1
Keijiro Takahashi <keijiro@gmail.com>
2015-12-12 17:56:34 +0900

    Add wire.

---
+++ Assets/Teatro/Wires/Wire.shader
@@ -0,0 +1,45 @@
+﻿Shader "Hidden/Teatro/Wire"
+{
+    Properties
+    {
+        [HDR] _Color ("Color", Color) = (1,1,1,1)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+
+        CGPROGRAM
+
+        #pragma surface surf Standard vertex:vert
+        #pragma target 3.0
+
+        #include "ClassicNoise2D.cginc"
+
+        struct Input {
+            float2 uv_MainTex;
+        };
+
+        half4 _Color;
+        float _Offset;
+
+        void vert(inout appdata_full v)
+        {
+            float2 np = float2(v.vertex.x * 4, _Time.y + _Offset);
+            v.vertex.y +=
+                max(0,
+                cnoise(np * 1) * 0.5 +
+                cnoise(np * 2) * 0.25 +
+                cnoise(np * 4) * 0.125) * 0.2;
+        }
+
+        void surf(Input IN, inout SurfaceOutputStandard o)
+        {
+            o.Albedo = 0;
+            o.Metallic = 0;
+            o.Smoothness = 0;
+            o.Emission = _Color;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
---
+++ Assets/Teatro/Wires/WireRenderer.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class WireRenderer : MonoBehaviour
+{
+    [SerializeField, ColorUsage(true, true, 0, 8, 0.125f, 3)]
+    Color _color = Color.white;
+
+    [SerializeField]
+    int _vertexCount = 128;
+
+    [HideInInspector]
+    [SerializeField]
+    Shader _shader;
+
+    Mesh _mesh;
+    Material _material;
+    float _offset;
+
+    Mesh BuildMesh()
+    {
+        var va = new Vector3[_vertexCount];
+        var ta = new Vector2[_vertexCount];
+
+        for (var i = 0; i < _vertexCount; i++)
+        {
+            var r = Mathf.PI * 2 * i / (_vertexCount - 1);
+            va[i] = new Vector3(Mathf.Cos(r), 0, Mathf.Sin(r));
+            ta[i] = new Vector2((float)i / (_vertexCount - 1), 0);
+        }
+
+        var ia = new int[_vertexCount + 1];
+
+        for (var i = 0; i < _vertexCount; i++) ia[i] = i;
+        ia[_vertexCount] = 0;
+
+        var mesh = new Mesh();
+        mesh.vertices = va;
+        mesh.uv = ta;
+        mesh.SetIndices(ia, MeshTopology.LineStrip, 0);
+        mesh.hideFlags = HideFlags.DontSave;
+        return mesh;
+    }
+
+    void SetUpResources()
+    {
+        if (_mesh) DestroyImmediate(_mesh);
+        if (_material) DestroyImmediate(_material);
+
+        _mesh = BuildMesh();
+
+        _material = new Material(_shader);
+        _material.hideFlags = HideFlags.DontSave;
+
+        _offset = Random.Range(-100.0f, 100.0f);
+    }
+
+    void Update()
+    {
+        if (_mesh == null) SetUpResources();
+
+        _material.SetColor("_Color", _color);
+        _material.SetFloat("_Offset", _offset);
+
+        var matrix = transform.localToWorldMatrix;
+        Graphics.DrawMesh(_mesh, matrix, _material, 0); 
+    }
+}
