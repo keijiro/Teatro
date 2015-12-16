af8dab920cf9332e439c6d462a4099b809cce963
Keijiro Takahashi <keijiro@ZX.local>
2015-12-17 01:06:59 +0900

    Add Ring renderer.

---
+++ Assets/Teatro/Ring/Ring.shader
@@ -0,0 +1,107 @@
+﻿Shader "Hidden/Teatro/Ring"
+{
+    Properties
+    {
+        _Color ("-", Color) = (1,1,1,1)
+        _Glossiness ("-", Range(0,1)) = 0.5
+        _Metallic ("-", Range(0,1)) = 0.0
+        [HDR] _Emission ("-", Color) = (1,1,1,1)
+    }
+
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+
+        CGPROGRAM
+
+        #pragma surface surf Standard vertex:vert nolightmap
+        #pragma target 3.0
+
+        #include "SimplexNoise2D.cginc"
+
+        struct Input { float dummy; };
+
+        fixed4 _Color;
+        half _Glossiness;
+        half _Metallic;
+        half3 _Emission;
+
+        float2 _Config; // throttle, random seed
+        half _Width;
+        half2 _Length;  // min, max
+        half2 _Radius;  // min, max
+        half _Slide;
+        half2 _Angles;  // arc angle, current angle
+        half3 _Noise;   // freq, offset x, y
+
+        // PRNG function
+        float nrand(float2 uv, float salt)
+        {
+            uv += float2(salt, _Config.y);
+            return frac(sin(dot(uv, float2(12.9898, 78.233))) * 43758.5453);
+        }
+
+        // Quaternion multiplication
+        // http://mathworld.wolfram.com/Quaternion.html
+        float4 qmul(float4 q1, float4 q2)
+        {
+            return float4(
+                q2.xyz * q1.w + q1.xyz * q2.w + cross(q1.xyz, q2.xyz),
+                q1.w * q2.w - dot(q1.xyz, q2.xyz)
+            );
+        }
+
+        // Vector rotation with a quaternion
+        // http://mathworld.wolfram.com/Quaternion.html
+        float3 rotate_vector(float3 v, float4 r)
+        {
+            float4 r_c = r * float4(-1, -1, -1, 1);
+            return qmul(r, qmul(float4(v, 0), r_c)).xyz;
+        }
+
+        // Rotation around the Y axis.
+        float4 y_rotation(float r)
+        {
+            return float4(0, sin(r * 0.5), 0, cos(r * 0.5));
+        }
+
+        void vert(inout appdata_full v)
+        {
+            float2 uv = v.texcoord.xy;
+
+            float sw = uv.x < _Config.x;
+
+            float phi = uv.x * _Angles.x;
+            phi += (nrand(uv.y, 0) - 0.5) * 2 * _Angles.y;
+
+            float2 np = float2(uv.x * _Noise.x + _Noise.y, uv.y + _Noise.z);
+            float ns = snoise(np) + 0.5 * snoise(np * 2);
+            ns = saturate(ns * 5);
+
+            float rad = lerp(_Radius.x, _Radius.y, nrand(uv.y, 1));
+            float sld = rad + _Slide * ns;
+
+            float len = lerp(_Length.x, _Length.y, nrand(uv.y, 2));
+
+            float3 p = v.vertex.xyz;
+
+            p.x *= _Width * (nrand(uv.y, 3) + 1) * 0.5;
+            p.z = p.z * len * lerp(0.5, 1, ns) + sld;
+
+            p = rotate_vector(p, y_rotation(phi));
+
+            v.vertex.xyz = p * sw;
+        }
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            o.Albedo = _Color.rgb;
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Emission = _Emission;
+        }
+
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
---
+++ Assets/Teatro/Ring/RingRenderer.cs
@@ -0,0 +1,174 @@
+using UnityEngine;
+
+namespace Teatro
+{
+    [ExecuteInEditMode]
+    public class RingRenderer : MonoBehaviour
+    {
+        #region Exposed Properties
+
+        [SerializeField, Range(0, 1)] float _throttle = 1.0f;
+
+        [Space]
+        [SerializeField] float _width = 0.02f;
+        [SerializeField] float _minLength = 0.01f;
+        [SerializeField] float _maxLength = 0.05f;
+
+        [Space]
+        [SerializeField] float _minRadius = 0.45f;
+        [SerializeField] float _maxRadius = 3.0f;
+        [SerializeField] float _slide = 0.2f;
+
+        [Space]
+        [SerializeField] float _arcAngle = 60.0f;
+        [SerializeField] float _rotationSpeed = 90.0f;
+
+        [Space]
+        [SerializeField] float _noiseFrequency = 4.0f;
+        [SerializeField] float _noiseMotion = 0.5f;
+
+        [Space]
+        [SerializeField] Color _albedoColor = Color.gray;
+        [SerializeField, Range(0, 1)] float _metallic = 0.5f;
+        [SerializeField, Range(0, 1)] float _smoothness = 0.5f;
+        [SerializeField, ColorUsage(true, true, 0, 8, 0.125f, 3)]
+        Color _emissionColor = Color.white;
+
+        [Space]
+        [SerializeField] float _randomSeed;
+
+        [SerializeField, HideInInspector] Shader _shader;
+
+        #endregion
+
+        #region Internal Objects and Variables
+
+        Mesh _mesh;
+        Material _material;
+
+        bool _needsReset = true;
+
+        float _rotation;
+        Vector2 _noiseOffset;
+
+        #endregion
+
+        #region MonoBehaviour Functions
+
+        void Update()
+        {
+            if (_needsReset)
+            {
+                ResetResources();
+                _needsReset = false;
+            }
+
+            _rotation += _rotationSpeed * Mathf.Deg2Rad * Time.deltaTime;
+            _noiseOffset.y += _noiseMotion * Time.deltaTime;
+
+            _material.SetVector("_Config", new Vector2(_throttle, _randomSeed));
+            _material.SetFloat("_Width", _width);
+            _material.SetVector("_Length", new Vector2(_minLength, _maxLength));
+            _material.SetVector("_Radius", new Vector2(_minRadius, _maxRadius));
+            _material.SetFloat("_Slide", _slide);
+            _material.SetVector("_Angles", new Vector2(_arcAngle * Mathf.Deg2Rad, _rotation));
+            _material.SetVector("_Noise", new Vector3(_noiseFrequency, _noiseOffset.x, _noiseOffset.y));
+            _material.SetColor("_Color", _albedoColor);
+            _material.SetFloat("_Metallic", _metallic);
+            _material.SetFloat("_Glossiness", _smoothness);
+            _material.SetColor("_Emission", _emissionColor);
+
+            Graphics.DrawMesh(_mesh, transform.localToWorldMatrix, _material, 0); 
+        }
+
+        #endregion
+
+        #region Resource Management
+
+        void ResetResources()
+        {
+            if (_mesh) DestroyImmediate(_mesh);
+            if (_material) DestroyImmediate(_material);
+
+            BuildBulkMesh();
+
+            _material = new Material(_shader);
+            _material.hideFlags = HideFlags.DontSave;
+
+            _rotation = 60.0f + _randomSeed * 30;
+            _noiseOffset = Vector3.one * _randomSeed * 11.1f;
+        }
+
+        void BuildBulkMesh()
+        {
+            var instanceCount = 65000 / 8;
+
+            var vtx_tmp = new Vector3[instanceCount * 8];
+            var nrm_tmp = new Vector3[vtx_tmp.Length];
+            var uv0_tmp = new Vector2[vtx_tmp.Length];
+
+            var i_tmp = 0;
+            for (var instance = 0; instance < instanceCount; instance++)
+            {
+                var uv0 = new Vector2(
+                    (float)(instance % 40) / 40,
+                    (float)(instance / 40) * 40 / instanceCount
+                );
+
+                vtx_tmp[i_tmp + 0] = vtx_tmp[i_tmp + 4] = new Vector3(-1, 0, -1);
+                vtx_tmp[i_tmp + 1] = vtx_tmp[i_tmp + 5] = new Vector3(+1, 0, -1);
+                vtx_tmp[i_tmp + 2] = vtx_tmp[i_tmp + 6] = new Vector3(-1, 0, +1);
+                vtx_tmp[i_tmp + 3] = vtx_tmp[i_tmp + 7] = new Vector3(+1, 0, +1);
+
+                nrm_tmp[i_tmp + 0] = nrm_tmp[i_tmp + 1] =
+                nrm_tmp[i_tmp + 2] = nrm_tmp[i_tmp + 3] = Vector3.up;
+                nrm_tmp[i_tmp + 4] = nrm_tmp[i_tmp + 5] =
+                nrm_tmp[i_tmp + 6] = nrm_tmp[i_tmp + 7] = -Vector3.up;
+
+                uv0_tmp[i_tmp + 0] = uv0_tmp[i_tmp + 1] =
+                uv0_tmp[i_tmp + 2] = uv0_tmp[i_tmp + 3] =
+                uv0_tmp[i_tmp + 4] = uv0_tmp[i_tmp + 5] =
+                uv0_tmp[i_tmp + 6] = uv0_tmp[i_tmp + 7] = uv0;
+
+                i_tmp += 8;
+            }
+
+            var idx_tmp = new int[12 * instanceCount];
+
+            i_tmp = 0;
+            var i_0 = 0;
+            for (var instance = 0; instance < instanceCount; instance++)
+            {
+                idx_tmp[i_tmp++] = i_0 + 0;
+                idx_tmp[i_tmp++] = i_0 + 2;
+                idx_tmp[i_tmp++] = i_0 + 1;
+
+                idx_tmp[i_tmp++] = i_0 + 3;
+                idx_tmp[i_tmp++] = i_0 + 1;
+                idx_tmp[i_tmp++] = i_0 + 2;
+
+                i_0 += 4;
+
+                idx_tmp[i_tmp++] = i_0 + 0;
+                idx_tmp[i_tmp++] = i_0 + 1;
+                idx_tmp[i_tmp++] = i_0 + 2;
+
+                idx_tmp[i_tmp++] = i_0 + 3;
+                idx_tmp[i_tmp++] = i_0 + 2;
+                idx_tmp[i_tmp++] = i_0 + 1;
+
+                i_0 += 4;
+            }
+
+            _mesh = new Mesh();
+            _mesh.hideFlags = HideFlags.DontSave;
+            _mesh.vertices = vtx_tmp;
+            _mesh.normals = nrm_tmp;
+            _mesh.uv = uv0_tmp;
+            _mesh.SetIndices(idx_tmp, MeshTopology.Triangles, 0);
+            _mesh.bounds = new Bounds(Vector3.zero, Vector3.one * 1000);
+        }
+
+        #endregion
+    }
+}
