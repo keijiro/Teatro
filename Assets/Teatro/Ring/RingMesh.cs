64aa1b46d6d887b14927d109547329eccd4e3b1d
Keijiro Takahashi <keijiro@gmail.com>
2015-12-29 15:29:14 +0900

    Add RingMesh asset class.

---
+++ Assets/Teatro/Ring/RingMesh.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Teatro
+{
+    public class RingMesh : ScriptableObject
+    {
+        #region Public Properties
+
+        [SerializeField, HideInInspector]
+        Mesh _mesh;
+
+        public Mesh sharedMesh {
+            get { return _mesh; }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void RebuildMesh()
+        {
+            if (_mesh == null)
+            {
+                Debug.LogError("Mesh asset is missing.");
+                return;
+            }
+
+            _mesh.Clear();
+
+            var instanceCount = 65000 / 8;
+
+            var vtx_tmp = new Vector3[instanceCount * 8];
+            var nrm_tmp = new Vector3[vtx_tmp.Length];
+            var uv0_tmp = new Vector2[vtx_tmp.Length];
+
+            var i_tmp = 0;
+            for (var instance = 0; instance < instanceCount; instance++)
+            {
+                var uv0 = new Vector2(
+                    (float)(instance % 40) / 40,
+                    (float)(instance / 40) * 40 / instanceCount
+                );
+
+                vtx_tmp[i_tmp + 0] = vtx_tmp[i_tmp + 4] = new Vector3(-1, 0, -1);
+                vtx_tmp[i_tmp + 1] = vtx_tmp[i_tmp + 5] = new Vector3(+1, 0, -1);
+                vtx_tmp[i_tmp + 2] = vtx_tmp[i_tmp + 6] = new Vector3(-1, 0, +1);
+                vtx_tmp[i_tmp + 3] = vtx_tmp[i_tmp + 7] = new Vector3(+1, 0, +1);
+
+                nrm_tmp[i_tmp + 0] = nrm_tmp[i_tmp + 1] =
+                nrm_tmp[i_tmp + 2] = nrm_tmp[i_tmp + 3] = Vector3.up;
+                nrm_tmp[i_tmp + 4] = nrm_tmp[i_tmp + 5] =
+                nrm_tmp[i_tmp + 6] = nrm_tmp[i_tmp + 7] = -Vector3.up;
+
+                uv0_tmp[i_tmp + 0] = uv0_tmp[i_tmp + 1] =
+                uv0_tmp[i_tmp + 2] = uv0_tmp[i_tmp + 3] =
+                uv0_tmp[i_tmp + 4] = uv0_tmp[i_tmp + 5] =
+                uv0_tmp[i_tmp + 6] = uv0_tmp[i_tmp + 7] = uv0;
+
+                i_tmp += 8;
+            }
+
+            var idx_tmp = new int[12 * instanceCount];
+
+            i_tmp = 0;
+            var i_0 = 0;
+            for (var instance = 0; instance < instanceCount; instance++)
+            {
+                idx_tmp[i_tmp++] = i_0 + 0;
+                idx_tmp[i_tmp++] = i_0 + 2;
+                idx_tmp[i_tmp++] = i_0 + 1;
+
+                idx_tmp[i_tmp++] = i_0 + 3;
+                idx_tmp[i_tmp++] = i_0 + 1;
+                idx_tmp[i_tmp++] = i_0 + 2;
+
+                i_0 += 4;
+
+                idx_tmp[i_tmp++] = i_0 + 0;
+                idx_tmp[i_tmp++] = i_0 + 1;
+                idx_tmp[i_tmp++] = i_0 + 2;
+
+                idx_tmp[i_tmp++] = i_0 + 3;
+                idx_tmp[i_tmp++] = i_0 + 2;
+                idx_tmp[i_tmp++] = i_0 + 1;
+
+                i_0 += 4;
+            }
+
+            _mesh.vertices = vtx_tmp;
+            _mesh.normals = nrm_tmp;
+            _mesh.uv = uv0_tmp;
+            _mesh.SetIndices(idx_tmp, MeshTopology.Triangles, 0);
+            _mesh.bounds = new Bounds(Vector3.zero, Vector3.one * 1000);
+
+            _mesh.Optimize();
+            _mesh.UploadMeshData(true);
+        }
+
+        #endregion
+
+        #region ScriptableObject Functions
+
+        void OnEnable()
+        {
+            if (_mesh == null)
+            {
+                _mesh = new Mesh();
+                _mesh.name = "Ring";
+            }
+        }
+
+        #endregion
+    }
+}
--- Assets/Teatro/Ring/RingRenderer.cs
+++ Assets/Teatro/Ring/RingRenderer.cs
@@ -35,6 +35,7 @@ namespace Teatro
         Color _emissionColor = Color.white;
 
         [Space]
+        [SerializeField] RingMesh _mesh;
         [SerializeField] float _randomSeed;
 
         [SerializeField, HideInInspector] Shader _shader;
@@ -53,11 +54,7 @@ namespace Teatro
 
         #region Internal Objects and Variables
 
-        Mesh _mesh;
         Material _material;
-
-        bool _needsReset = true;
-
         float _rotation;
         Vector2 _noiseOffset;
 
@@ -65,12 +62,23 @@ namespace Teatro
 
         #region MonoBehaviour Functions
 
+        void Start()
+        {
+            _rotation = 60.0f + _randomSeed * 30;
+            _noiseOffset = Vector3.one * _randomSeed * 11.1f;
+        }
+
+        void OnDestroy()
+        {
+            if (_material) DestroyImmediate(_material);
+        }
+
         void Update()
         {
-            if (_needsReset)
+            if (_material == null)
             {
-                ResetResources();
-                _needsReset = false;
+                _material = new Material(_shader);
+                _material.hideFlags = HideFlags.DontSave;
             }
 
             _rotation += _rotationSpeed * Mathf.Deg2Rad * Time.deltaTime;
@@ -88,95 +96,7 @@ namespace Teatro
             _material.SetFloat("_Glossiness", _smoothness);
             _material.SetColor("_Emission", _emissionColor);
 
-            Graphics.DrawMesh(_mesh, transform.localToWorldMatrix, _material, 0); 
-        }
-
-        #endregion
-
-        #region Resource Management
-
-        void ResetResources()
-        {
-            if (_mesh) DestroyImmediate(_mesh);
-            if (_material) DestroyImmediate(_material);
-
-            BuildBulkMesh();
-
-            _material = new Material(_shader);
-            _material.hideFlags = HideFlags.DontSave;
-
-            _rotation = 60.0f + _randomSeed * 30;
-            _noiseOffset = Vector3.one * _randomSeed * 11.1f;
-        }
-
-        void BuildBulkMesh()
-        {
-            var instanceCount = 65000 / 8;
-
-            var vtx_tmp = new Vector3[instanceCount * 8];
-            var nrm_tmp = new Vector3[vtx_tmp.Length];
-            var uv0_tmp = new Vector2[vtx_tmp.Length];
-
-            var i_tmp = 0;
-            for (var instance = 0; instance < instanceCount; instance++)
-            {
-                var uv0 = new Vector2(
-                    (float)(instance % 40) / 40,
-                    (float)(instance / 40) * 40 / instanceCount
-                );
-
-                vtx_tmp[i_tmp + 0] = vtx_tmp[i_tmp + 4] = new Vector3(-1, 0, -1);
-                vtx_tmp[i_tmp + 1] = vtx_tmp[i_tmp + 5] = new Vector3(+1, 0, -1);
-                vtx_tmp[i_tmp + 2] = vtx_tmp[i_tmp + 6] = new Vector3(-1, 0, +1);
-                vtx_tmp[i_tmp + 3] = vtx_tmp[i_tmp + 7] = new Vector3(+1, 0, +1);
-
-                nrm_tmp[i_tmp + 0] = nrm_tmp[i_tmp + 1] =
-                nrm_tmp[i_tmp + 2] = nrm_tmp[i_tmp + 3] = Vector3.up;
-                nrm_tmp[i_tmp + 4] = nrm_tmp[i_tmp + 5] =
-                nrm_tmp[i_tmp + 6] = nrm_tmp[i_tmp + 7] = -Vector3.up;
-
-                uv0_tmp[i_tmp + 0] = uv0_tmp[i_tmp + 1] =
-                uv0_tmp[i_tmp + 2] = uv0_tmp[i_tmp + 3] =
-                uv0_tmp[i_tmp + 4] = uv0_tmp[i_tmp + 5] =
-                uv0_tmp[i_tmp + 6] = uv0_tmp[i_tmp + 7] = uv0;
-
-                i_tmp += 8;
-            }
-
-            var idx_tmp = new int[12 * instanceCount];
-
-            i_tmp = 0;
-            var i_0 = 0;
-            for (var instance = 0; instance < instanceCount; instance++)
-            {
-                idx_tmp[i_tmp++] = i_0 + 0;
-                idx_tmp[i_tmp++] = i_0 + 2;
-                idx_tmp[i_tmp++] = i_0 + 1;
-
-                idx_tmp[i_tmp++] = i_0 + 3;
-                idx_tmp[i_tmp++] = i_0 + 1;
-                idx_tmp[i_tmp++] = i_0 + 2;
-
-                i_0 += 4;
-
-                idx_tmp[i_tmp++] = i_0 + 0;
-                idx_tmp[i_tmp++] = i_0 + 1;
-                idx_tmp[i_tmp++] = i_0 + 2;
-
-                idx_tmp[i_tmp++] = i_0 + 3;
-                idx_tmp[i_tmp++] = i_0 + 2;
-                idx_tmp[i_tmp++] = i_0 + 1;
-
-                i_0 += 4;
-            }
-
-            _mesh = new Mesh();
-            _mesh.hideFlags = HideFlags.DontSave;
-            _mesh.vertices = vtx_tmp;
-            _mesh.normals = nrm_tmp;
-            _mesh.uv = uv0_tmp;
-            _mesh.SetIndices(idx_tmp, MeshTopology.Triangles, 0);
-            _mesh.bounds = new Bounds(Vector3.zero, Vector3.one * 1000);
+            Graphics.DrawMesh(_mesh.sharedMesh, transform.localToWorldMatrix, _material, 0); 
         }
 
         #endregion
